37e17129a297b73c101ad8f3acac784d8c4f5bc4
samonzeweb <sam.onzeweb@gmail.com>
2020-12-05 17:03:34 +0100

    Added set/get for pieces in board

--- src/rusthello.rs
+++ src/rusthello.rs
@@ -4,7 +4,7 @@ pub struct Board {
 }
 
 /// Players.
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, Debug, PartialEq)]
 pub enum Player {
     Black,
     White
@@ -15,6 +15,25 @@ impl Board {
     pub fn new() -> Board {
         Board{
             cells: [[None; 8]; 8]
+        }
+    }
+
+    pub fn set_piece(&mut self, x: usize, y: usize, piece: Option<Player>) -> Result<(), String> {
+        self.check_coordonates(x, y)?;
+        self.cells[x][y] = piece;
+        Ok(())
+    }
+
+    pub fn get_piece(&self, x: usize, y:usize) -> Result<Option<Player>, String> {
+        self.check_coordonates(x, y)?;
+        Ok(self.cells[x][y])
+    }
+
+    fn check_coordonates(&self, x: usize, y: usize) -> Result<(), String> {
+        if x > 7 || y > 7 {
+            Err(format!("the given coordinates are out of range"))
+        } else {
+            Ok(())
         }
     }
 }
@@ -30,5 +49,20 @@ mod tests {
                 .for_each(|content|  {
                     assert_eq!(content.is_none(), true);
                 })
+    }
+
+    #[test]
+    fn set_piece() {
+        let mut board = Board::new();
+        board.set_piece(1,2, Some(Player::Black)).unwrap();
+        assert_eq!(board.cells[1][2], Some(Player::Black))
+    }
+
+    #[test]
+    fn get_piece() {
+        let mut board = Board::new();
+        board.cells[3][4] = Some(Player::White);
+        let piece = board.get_piece(3,4).unwrap();
+        assert_eq!(piece, Some(Player::White))
     }
 }
