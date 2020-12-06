e69a64c5c9d0f05d53f109d4b59c7c320679968b
samonzeweb <sam.onzeweb@gmail.com>
2020-12-06 10:04:51 +0100

    Added coordinates to the ascii board

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
+use io::Stdout;
+use std::io::{self, Write};
 use rusthello;
 
 fn main() {
     let board = rusthello::Board::new_start();
-    println!("Hello, world!");
+    io::stdout().write(rusthello::board_to_ascii(&board).as_bytes()).unwrap();
 }
--- src/rusthello/ascii_board.rs
+++ src/rusthello/ascii_board.rs
@@ -1,12 +1,15 @@
 use crate::{Board, Player};
 
-const ROW_REPARATOR: &str = "+---+---+---+---+---+---+---+---+\n";
+const ROW_REPARATOR: &str = "  +---+---+---+---+---+---+---+---+\n";
+const LETTERS: &str =       "    A   B   C   D   E   F   G   H\n";
 
 /// Build an ascii representation of a board
 pub fn board_to_ascii(board: &Board) -> String {
     let mut ascii = String::new();
+    ascii.push_str(LETTERS);
     for y in 0..8 {
         ascii.push_str(ROW_REPARATOR);
+        ascii.push_str(format!("{} ", y + 1).as_str());
         for x in 0..8 {
             let piece = board.get_piece(x, y).unwrap();
             ascii.push_str(cell_to_ascii(piece));
@@ -31,24 +34,29 @@ mod tests {
     use super::*;
     #[test]
     fn board_to_ascii_produce_ascii_representation_of_a_board() {
-        let expected = "+---+---+---+---+---+---+---+---+\n\
-                             |   |   |   |   |   |   |   |   |\n\
-                             +---+---+---+---+---+---+---+---+\n\
-                             |   |   |   |   |   |   |   |   |\n\
-                             +---+---+---+---+---+---+---+---+\n\
-                             |   |   |   |   |   |   |   |   |\n\
-                             +---+---+---+---+---+---+---+---+\n\
-                             |   |   |   | O | X |   |   |   |\n\
-                             +---+---+---+---+---+---+---+---+\n\
-                             |   |   |   | X | O |   |   |   |\n\
-                             +---+---+---+---+---+---+---+---+\n\
-                             |   |   |   |   |   |   |   |   |\n\
-                             +---+---+---+---+---+---+---+---+\n\
-                             |   |   |   |   |   |   |   |   |\n\
-                             +---+---+---+---+---+---+---+---+\n\
-                             |   |   |   |   |   |   |   |   |\n\
-                             +---+---+---+---+---+---+---+---+\n";
+        // The dots aren't parts of the expected board representation.
+        // They're purpose is to manage alignment, there are removed
+        // before the comparison.
+        let expected = "    A   B   C   D   E   F   G   H\n\
+                             . +---+---+---+---+---+---+---+---+\n\
+                             1 |   |   |   |   |   |   |   |   |\n\
+                             . +---+---+---+---+---+---+---+---+\n\
+                             2 |   |   |   |   |   |   |   |   |\n\
+                             . +---+---+---+---+---+---+---+---+\n\
+                             3 |   |   |   |   |   |   |   |   |\n\
+                             . +---+---+---+---+---+---+---+---+\n\
+                             4 |   |   |   | O | X |   |   |   |\n\
+                             . +---+---+---+---+---+---+---+---+\n\
+                             5 |   |   |   | X | O |   |   |   |\n\
+                             . +---+---+---+---+---+---+---+---+\n\
+                             6 |   |   |   |   |   |   |   |   |\n\
+                             . +---+---+---+---+---+---+---+---+\n\
+                             7 |   |   |   |   |   |   |   |   |\n\
+                             . +---+---+---+---+---+---+---+---+\n\
+                             8 |   |   |   |   |   |   |   |   |\n\
+                             . +---+---+---+---+---+---+---+---+\n";
 
+        let expected = expected.replace(".", " ");
         let board = Board::new_start();
         let ascii = board_to_ascii(&board);
         assert_eq!(ascii, expected);        
