0d2e4eabec9e374a1ae704885e4c18875a3db7b3
samonzeweb <sam.onzeweb@gmail.com>
2020-12-19 17:07:13 +0100

    GameStatus is reusable

--- src/rusthello/game.rs
+++ src/rusthello/game.rs
@@ -1,5 +1,5 @@
 use super::board::*;
-
+use super::game_status::*;
 /// Manage an Othello game workflow
 pub struct Game {
     board: Board,
@@ -87,57 +87,10 @@ impl Game {
     }
 
     pub fn count_pieces(&self) -> (u8, u8) {
-        (self.status.black_pieces, self.status.white_pieces)
-    }
-}
-
-#[derive(Default)]
-struct GameStatus {
-    black_can_move: bool,
-    white_can_move: bool,
-    black_pieces: u8,
-    white_pieces: u8,
-}
-
-impl GameStatus {
-    fn evaluate_board(board: &Board) -> Self {
-        let mut black_can_move = false;
-        let mut white_can_move = false;
-        let (black_pieces, white_pieces) = board.count_pieces();
-        if (black_pieces + white_pieces) != 64 {
-            black_can_move = board.can_player_move(Player::Black);
-            white_can_move = board.can_player_move(Player::White);
-        }
-
-        Self {
-            black_can_move,
-            white_can_move,
-            black_pieces,
-            white_pieces,
-        }
-    }
-
-    fn can_player_move(&self, player: Player) -> bool {
-        match player {
-            Player::Black => self.black_can_move,
-            Player::White => self.white_can_move,
-        }
-    }
-
-    fn game_over(&self) -> bool {
-        !self.black_can_move && !self.white_can_move
-    }
-
-    fn winner(&self) -> Option<Player> {
-        if !self.game_over() || self.black_pieces == self.white_pieces {
-            None
-        } else {
-            if self.black_pieces > self.white_pieces {
-                Some(Player::Black)
-            } else {
-                Some(Player::White)
-            }
-        }
+        (
+            self.status.pieces_count(Player::Black),
+            self.status.pieces_count(Player::White),
+        )
     }
 }
 
